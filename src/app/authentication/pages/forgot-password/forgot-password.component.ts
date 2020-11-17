449b5c79f09cde6d770b6310a2da59fed5587acf
ahmed ezzat <ahm.ezzat.ali@gmail.com>
2020-11-17 20:52:31 +0200

    forget password api call

--- src/app/authentication/pages/forgot-password/forgot-password.component.ts
+++ src/app/authentication/pages/forgot-password/forgot-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CommonService } from 'src/app/services/common/common.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'md-forgot-password',
@@ -10,21 +12,29 @@ export class ForgotPasswordComponent implements OnInit {
 
   email: string = null;
   emailSent = false;
-  constructor(private snackBar: MatSnackBar) { }
+  constructor(private snackBar: MatSnackBar, private authService: AuthService, public commonService: CommonService) { }
 
   ngOnInit(): void {
   }
 
-  sendEmail(){
+  sendEmailNotification() {
+    this.commonService.showLoading();
+    this.authService.forgotPassword(this.email).subscribe(res => {
+      debugger
+      if (res.success) {
+        this.snackBar.open('Please check your inbox, we have sent you and email with the new password.', null,
+          {
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+          });
+        this.emailSent = true;
+      }
+      this.commonService.hideLoading();
+    })
 
-    this.snackBar.open('Please check your inbox, we have sent you and email with the new password.', null,
-      {
-          duration: 2000,
-          horizontalPosition: 'center',
-          verticalPosition: 'top',
-      });
 
-      this.emailSent = true;
+
 
   }
 
--- src/app/authentication/services/auth.service.ts
+++ src/app/authentication/services/auth.service.ts
@@ -12,12 +12,18 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
 
+  register(body: RegistrationModel): Observable<any> {
+    return this.http.post('/services/app/Account/Register', body);
+  }
+
   login(body: LoginModel): Observable<any> {
     return this.http.post('/TokenAuth/Authenticate', body);
   }
 
-  register(body: RegistrationModel): Observable<any> {
-    return this.http.post('/services/app/Account/Register', body);
+  forgotPassword(email: string): Observable<any> {
+    return this.http.post('/services/app/Notification/SendForgetPassword?' + email, null);
   }
+
+
  
 }
